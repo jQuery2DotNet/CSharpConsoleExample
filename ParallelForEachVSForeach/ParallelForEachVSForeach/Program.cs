462b0d9655c96a09a2a0c58457d0c73b9c616944
J2DN <J2DN@DESKTOP-JNFU1KH>
2016-07-15 20:32:59 -0700

    add parallel for each vs foreach

---
+++ ParallelForEachVSForeach/ParallelForEachVSForeach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelForEachVSForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = { "1. Red", "2. Green", "3. Blue", "4. Yellow", "5. White", "6. Black", "7. Violet", "8. Brown", "9. Orange", "10. Pink" };
+
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("\nTraditional foreach loop");
+            // start the stopwatch for "for" loop
+            foreach (string color in colors)
+            {
+                Console.WriteLine("{0}, Thread Id= {1}", color, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+            Console.WriteLine("foreach loop execution time = {0} seconds\n", sw.Elapsed.TotalSeconds);
+
+
+            Console.WriteLine("Using Parallel.ForEach");
+            //start the stopwatch for "Parallel.ForEach"
+            sw = Stopwatch.StartNew();
+            Parallel.ForEach(colors, color =>
+            {
+                Console.WriteLine("{0}, Thread Id= {1}", color, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            });
+            Console.WriteLine("Parallel.ForEach() execution time = {0} seconds", sw.Elapsed.TotalSeconds);
+
+            Console.ReadLine();
+        }
+    }
+}
